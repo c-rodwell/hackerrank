67b5ed74a593df56efd5599edd5791a2efd5e1b7
Colin <rodwell.colin@gmail.com>
2020-05-13 20:05:14 -0700

    more robot approaches - cut down non-optimal routes by checking all 'take score' cases agains the 'take energy' case, keep only if it has better energy or score. robot_forward_or_first is fastest, and_first is close

---
+++ robot/robot_forward_and_first.py
@@ -0,0 +1,59 @@
+#try some ways to cut down non-optimal routes.
+#first - eliminate "take score" cases which are not better than take_energy case
+
+def best_total_score(config):
+	N, V, P = config
+	routes = [(0,0)] #start state is step = 1(implied), energy = 0, accumulated score = 0
+	for step in range(1,N): #step = 1 through step = N - 1 : 2 cases, lose if energy becomes 0
+		
+		#all routes fail - will happen if P has 0 at beginning, or too many 0 in a row.
+		if not routes:
+			print("all routes failed")
+			return None
+
+		#newRoutes = []
+		
+		this_score = V[step-1]
+		this_energy = P[step-1]
+
+		# all "take score" cases which are not better than each other continue to not be better than each other
+		
+		# V can have  - then the "take energy" branch loses immediately.
+		if this_energy > 0:
+
+			#"take energy" option: energy becomes the same whatever it was before. so all states going to it become one, score is max of their scores.
+
+			all_max_score = max([route_max_score for (energy, route_max_score) in routes])
+			takeEnergy = (this_energy-1, all_max_score)
+
+			#then add "pick score" ones - only if they dont lose (e > 0) and are better than the "take energy" case.
+			newRoutes = [(energy-1, route_max_score+this_score) for (energy, route_max_score) in routes if (energy > 0 and (energy > this_energy or route_max_score+this_score > all_max_score))]
+			newRoutes.append(takeEnergy)
+
+		else:
+			newRoutes = [(energy-1, route_max_score+this_score) for (energy, route_max_score) in routes if energy > 0]
+
+		routes = newRoutes
+
+	#step = N: only the plus score case, no energy >0 check
+	this_score = V[N-1]
+	return max(route_max_score for (energy, route_max_score) in routes) + this_score
+
+#intended: add state if better or non-comparable, but not equal or worse, to takeEnergy.
+# def notDominatedBy(tuple1, tuple2):
+# 	x1, y1 = tuple1
+# 	x2, y2 = tuple2
+# 	return (x1 > x2) or (y1 > y2)
+
+def robot(vp):
+
+    N = len(vp)
+    V = []
+    P = []
+    for [vi, pi] in vp:
+        V.append(vi)
+        P.append(pi)
+    
+    config = (N,V,P)
+
+    return best_total_score(config)
--- robot/test.py
+++ robot/test.py
@@ -7,10 +7,16 @@ import robot_tree_stack_contains
 import robot_tree_wasqueued
 import robot_forward
 import robot_forward_comprehensions
+import robot_forward_comprehensions_reduce
+import robot_forward_and_first
+import robot_forward_or_first
 
 start = (1,0)
 conf1 = (4, [4,0,4,3], [2,2,0,4])
 
+max_N = 5*pow(10,5)
+max_V = pow(10,9)
+max_P = pow(10,5)
 
 #random parameters in the hackerRank format, up to some maxes.
 #bounds are 1 <= N <= 5x10^5, 0 <= Vi <= 10^9 , 0 <= Pi <= 10^5
@@ -32,7 +38,13 @@ def testRandom(N, maxV, maxP):
     # methods = [("no check for duplicates in stack", robot_tree_no_check.robot),
     #             ("direct forward iteration", robot_forward.robot)]
 
-    methods = [("direct forward iteration", robot_forward.robot), ("direct forward with comphrehensions", robot_forward_comprehensions.robot)]
+    # methods = [("direct forward iteration", robot_forward.robot),
+    #             ("direct forward with comphrehensions", robot_forward_comprehensions.robot),
+    #             ("comperehensions, comparing all to energy case", robot_forward_comprehensions_reduce.robot)]
+
+    methods = [("comperehensions, comparing all to energy case", robot_forward_comprehensions_reduce.robot),
+                ("compare inside comprehension, and first", robot_forward_and_first.robot),
+                ("compare inside comprehension, or first", robot_forward_or_first.robot)]
 
     for name, function in methods:
         print("\nmethod: "+name)
@@ -41,3 +53,28 @@ def testRandom(N, maxV, maxP):
         elapsed = time.time() - before
         print("result = "+str(result))
         print("time = "+str(elapsed))
+
+
+def repeatTest(N, func, args):
+    print("repetition test on func"+str(args)+", N = "+str(N))
+    before = time.time()
+    for i in range(N):
+        func(*args)
+    elapsed = time.time() - before
+    print("total time = "+str(elapsed))
+    print("average time = "+str(elapsed / N))
+
+def compare1(tuple1, tuple2):
+    x1, y1 = tuple1
+    x2, y2 = tuple2
+    return (x1 > x2) or (y1 > y2)
+
+def compare2(tuple1, tuple2):
+    (x1, y1), (x2, y2) = tuple1, tuple2
+    return (x1 > x2) or (y1 > y2)
+
+def compare3(x1, y1, x2, y2):
+    return (x1 > x2) or (y1 > y2)
+
+def compare4(t1, t2):
+    return (t1[0] > t2[0]) or (t1[1] > t2[1])
