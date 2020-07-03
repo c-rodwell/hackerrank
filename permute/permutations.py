4d01329fd0206f2659c1c31d63fdbb3ac9ec5abb
Colin <rodwell.colin@gmail.com>
2020-07-02 21:49:44 -0700

    permutations: add recursive version make length argument optional on all versions 	TODO: length param is used differently in each version, should 	make consistent. 	also should make return type consistent- tuple vs lists

--- permute/permutations.py
+++ permute/permutations.py
@@ -2,7 +2,9 @@ import math
 import time
 import itertools
 
-#permutations algorithms.
+#permutations algorithms: generate all permutations on a list
+#note the implementations use optional length arg differently, but should match at length=None
+#havent fixed this since I'm mainly looking at the full list permutations
 
 #my first idea- iterative, at iteration n we have all permutations of length n
 #maybe inefficient since many nested loops 
@@ -22,10 +24,32 @@ def looping_permute(items, length = None):
 					permutations[i].append(p+tuple([item]))
 	return permutations[length]
 
+#recursive approach: pick one element, then permute the rest.
+#multiple choices for first: must try all of them
+#permuting the rest generates a list: need to add first element to all of those
+
+def recursive_permute(items, length=None):
+	if length:
+		return recursive_permute(items)
+	#base case: empty list
+	if not items:
+		return [[]]
+	output = []
+	for item in items:
+		remaining = items[:]
+		remaining.remove(item)
+		remaining_permutations = recursive_permute(remaining)
+		output += [[item]+permutation for permutation in remaining_permutations]
+	return output
+
 #my attempt at permutations by swapping in place
 #correct up to 5, but failing on 6
 #not the same as Heap's algorithm which only does single swaps
-def cycles_permute(items, length):
+def cycles_permute(items, length=None):
+	if length is None:
+		length = len(items)
+	elif length > len(items):
+		raise ValueError("length parameter must be less than or equal to length of array")
 	#permuting one element does nothing
 	if length <= 1:
 		return [items[:]]
@@ -62,7 +86,11 @@ def cycle_inplace(items, n, direction):
 
 #itertools.permutations() returns an iterable of tuples, which is probably better than my list of lists format
 #turn that into a list of lists so I can check is_correct_permutations_list
-def itertools_permutations(items, length):
+def itertools_permutations(items, length=None):
+	if length is None:
+		length = len(items)
+	elif length > len(items):
+		raise ValueError("length parameter must be less than or equal to length of array")
 	return [list(x) for x in itertools.permutations(items, length)]
 
 
@@ -94,11 +122,12 @@ def is_correct_permutations_list(candidate, base):
 
 CURRENT_METHODS = [
 	("looping permute", looping_permute),
+	("recursive permute", recursive_permute),
 	("recursive permute with cycles", cycles_permute),
 	("itertools.permutations, as list of lists", itertools_permutations)
 	]
 
-BASE_SIZES = [1,2,3,4,5,6,7]
+BASE_SIZES = [1,2,3,4,5,6,7,10,15,20,25,30]
 
 def compareAll(methods = CURRENT_METHODS, sizes = BASE_SIZES, showLen = 200):
 
@@ -110,7 +139,7 @@ def compareAll(methods = CURRENT_METHODS, sizes = BASE_SIZES, showLen = 200):
 		for name, method in methods:
 			print("\n"+name+":")
 			before = time.time()
-			answer = method(base, size)
+			answer = method(base)
 			elapsed = time.time() - before
 			correct = is_correct_permutations_list(answer, base)
 			if len(answer) < showLen:
